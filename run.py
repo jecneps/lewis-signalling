4ccfe562a2b4e57df81682bc053df003094045ba
jecneps <jejcneps@gmail.com>
2020-03-06 14:54:31 -0500

    test setup

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from urns import Reinforcement
 reinforcement = Reinforcement(2, 0.99)
 sim = Simulation(reinforcement)
 #sim.run(10000)
-#sim.recordUrnLevels(100, 1000000, "n3UrnLevels.p")
-sim.recordStateAddition(1, 200000, 2, 7,"stateAddition.p")
+sim.recordUrnLevels(100, 100, "test.p")
+#sim.recordStateAddition(1, 200000, 2, 7,"stateAddition.p")
