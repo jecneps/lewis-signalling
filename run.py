9723edd6a9b5b1035767ec838c0e7a89a320add2
jecneps <jejcneps@gmail.com>
2020-03-12 18:41:51 -0400

    moved assests

--- run.py
+++ run.py
@@ -2,9 +2,22 @@ from sim import Simulation
 from urns import Reinforcement 
 
 
-reinforcement = Reinforcement(7, 0.99)
+reinforcement = Reinforcement(2, 0.99)
 sim = Simulation(reinforcement)
 #sim.run(10000)
-#sim.recordUrnLevels(100, 100000, "test.p")
+print("levels for 2")
+sim.recordUrnLevels(100, 100000, "u2Levels.p")
+
+reinforcement = Reinforcement(3, 0.99)
+sim = Simulation(reinforcement)
+#sim.run(10000)
+print("levels for 3")
+sim.recordUrnLevels(100, 100000, "u3Levels.p")
+
+reinforcement = Reinforcement(4, 0.99)
+sim = Simulation(reinforcement)
+#sim.run(10000)
+print("levels for 4")
+sim.recordUrnLevels(100, 100000, "u4Levels.p")
 #sim.recordStateAddition(1, 200000, 2, 7,"stateAddition.p")
-sim.recordSimulations(1000, 1000000, "n7Urns.p")
+#sim.recordSimulations(1000, 1000000, "n7Urns.p")
